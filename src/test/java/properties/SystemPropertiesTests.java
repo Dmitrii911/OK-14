d634296a0dec776f07c5e4e25ab46e9fd29b417b
Dmitrii Oleinik <wedil@mail.ru>
2025-10-06 23:54:39 +0500

    Add all

---
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -0,0 +1,19 @@
+package properties;
+
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+
+    @Test
+  // @Tag("property")
+    void systemProperties4Test() {
+        String browser = System.getProperty("browser", "firefox");
+
+        System.out.println(browser);
+        // gradle property_test
+        // firefox
+
+        // gradle property_test -Dbrowser=chrome
+        // chrome
+    }
+}
