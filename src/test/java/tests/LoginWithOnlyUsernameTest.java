da08848660f6e9a92b07340225c6f9430b57c7f2
Dmitrii Oleinik <wedil@mail.ru>
2025-10-18 23:42:16 +0500

    Add SupportTest

---
+++ src/test/java/tests/LoginWithOnlyUsernameTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.LoginPage;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginWithOnlyUsernameTest extends TestBase {
+    private static LoginPage loginPage;
+
+    @BeforeEach
+    public void prepare(){
+        open(baseUrl);
+        //Принятие куки и политики
+        loginPage = new LoginPage();
+//        loginPage.acceptCookie();
+//        loginPage.acceptPrivacyButton();
+
+    }
+    @Test
+    @Tag("ok")
+    @DisplayName("Тест входа только по имени пользователя")
+    public void loginWithUsernameTest(){
+        //Попытка входа с логином
+        loginPage.loginWithUsername("user");
+        //проверка наличия сообщения об ошибке
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+        // Проверка текста сообщения об ошибке
+        String expectedErrorMessage = "Enter password";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "текст об ошибке не совпадает");
+
+    }
+}
