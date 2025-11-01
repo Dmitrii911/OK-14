cefde174bc1ea422acae5de34412c333c8614edf
Dmitrii Oleinik <wedil@mail.ru>
2025-11-01 23:15:43 +0500

    Add corrections in class

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,39 +1,12 @@
 package pages;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import io.qameta.allure.Step;
-
-import static com.codeborne.selenide.Selenide.$;
 
 public abstract class BasePage {
-    //Примеры общих элементов, которые могут использоваться на различных страницах
     protected SelenideElement headerLogo = Selenide.$("[tsid='toolbar_logo']");
     protected SelenideElement searchField = Selenide.$("[name='st.query']");
     protected SelenideElement vkServices = Selenide.$("[data-l='t,vk_ecosystem']");
     protected SelenideElement acceptPrivacyButton = Selenide.$(".cmptxt_btn_yes");
-
-    @Step("Метод поиска по сайту")
-    public void search(String query) {
-        searchField.shouldBe(Condition.visible).setValue(query).pressEnter();
-    }
-
-    @Step("Открываем VK Services")
-    public void openVkServices() {
-        vkServices.shouldBe(Condition.visible).click();
-    }
-
-    @Step("Клик на логотип ОК")
-    public void clickLogo() {
-        headerLogo.shouldBe(Condition.visible).click();
-
-    }
-
-    @Step("Принимаем конфиденциальности")
-    public void acceptPrivacyButton() {
-        acceptPrivacyButton.shouldBe(Condition.visible).click();
-
-    }
 }
 
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -17,15 +17,12 @@ public class LoginPage extends BasePage {
     private SelenideElement restoreButton = $(".lp");
     private SelenideElement inputSearch = $(".search-wrapper__4nym2");
 
-    //Локаторы для кнопок соцсетей
     private SelenideElement vkButton = $("[data-l = 't,vkc']");
     private SelenideElement yandexButton = $(".social-icon");
     private SelenideElement mailRuButton = $("[data-l = 't,mailru']");
 
-    //Локатор для элемента текста с ошибкой
     private SelenideElement errorMessage = $(".input-e.login_error");
 
-    // Локатор для восстановления пароля
     private SelenideElement goToRecoveryButton = $("[tsid=\"restore\"]");
 
     {
@@ -86,32 +83,6 @@ public class LoginPage extends BasePage {
     public void loginWithPassword(String password) {
         passwordField.shouldBe(visible).setValue(password);
         loginButton.shouldBe(visible).click();
-    }
-
-    @Step("Переходим наа страницу восстановления пароля")
-    public void openForgotPasswordPage() {
-        forgotPasswordLink.shouldBe(visible).click();
-    }
-
-    @Step("Переходим наа страницу регистрации")
-    public void openRegistrationPage() {
-        registrationButton.shouldBe(visible).click();
-    }
-
-    //Методы для перехода для регистрации через страницы соцсети
-    @Step("Вход через ВК")
-    public void openwithVK() {
-        vkButton.shouldBe(visible).click();
-    }
-
-    @Step("Вход через google")
-    public void openwithGoogle() {
-        yandexButton.shouldBe(visible).click();
-    }
-
-    @Step("Вход через mail.ru")
-    public void openwithMailRu() {
-        mailRuButton.shouldBe(visible).click();
     }
 
     @Step("Кликаем на вход по кр-коду")
