b88cf7003e1fcb85252093ca3d35279a7856d1db
Dmitrii Oleinik <wedil@mail.ru>
2025-11-02 22:56:48 +0500

    Add corrections in class

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class LoginPage extends BasePage {
-    private SelenideElement usernameField = $("[name = 'st.email']");
+    private SelenideElement usernameField = $(".field_email");
     private SelenideElement passwordField = $("[name = 'st.password']");
     private SelenideElement loginButton = $("[tsid=\"login-block21_input_9ad800\"]");
     private SelenideElement forgotPasswordLink = $("[data-l = 't,restore']");
@@ -16,34 +16,135 @@ public class LoginPage extends BasePage {
     private SelenideElement qrCodeButton = $(".qr-code-button");
     private SelenideElement restoreButton = $(".lp");
     private SelenideElement inputSearch = $(".search-wrapper__4nym2");
-
     private SelenideElement vkButton = $("[data-l = 't,vkc']");
     private SelenideElement yandexButton = $(".social-icon");
     private SelenideElement mailRuButton = $("[data-l = 't,mailru']");
-
     private SelenideElement errorMessage = $(".input-e.login_error");
-
     private SelenideElement goToRecoveryButton = $("[tsid=\"restore\"]");
 
     {
-        verifyPageElements();
+        verifyUsernameFieldIsVisible();
+        verifyPasswordFieldIsVisible();
+        verifyLoginButtonIsVisible();
+        verifyForgotPasswordLinkIsVisible();
+        verifyRegistrationButtonIsVisible();
+        verifyVkAuthButtonIsVisible();
+        verifyYandexAuthButtonIsVisible();
+        verifyMailRuAuthButtonIsVisible();
+        verifyQrCodeButtonIsVisible();
+        verifyRestoreButtonIsVisible();
+        verifyInputSearchIsVisible();
+
     }
 
-    @Step("Проверяем видимость всех элементов страницы")
-    private void verifyPageElements() {
-        usernameField.shouldBe(visible);
-        passwordField.shouldBe(visible);
-        loginButton.shouldBe(visible);
-        forgotPasswordLink.shouldBe(visible);
-        registrationButton.shouldBe(visible);
-        vkButton.shouldBe(visible);
-        yandexButton.shouldBe(visible);
-        mailRuButton.shouldBe(visible);
-        qrCodeButton.shouldBe(visible);
-        restoreButton.shouldBe(visible);
-        inputSearch.shouldBe(visible);
+    @Step("Проверяем видимость поля ввода имени пользователя")
+    private void verifyUsernameFieldIsVisible() {
+        try {
+            usernameField.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости поля ввода имени пользователя");
+            throw new AssertionError(e.getMessage());
+        }
+    }
 
+    @Step("Проверяем видимость поля ввода пароля")
+    public void verifyPasswordFieldIsVisible() {
+        try {
+            passwordField.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости поля ввода пароля");
+            throw new AssertionError(e.getMessage());
+        }
+    }
 
+    @Step("Проверяем видимость кнопки входа")
+    public void verifyLoginButtonIsVisible() {
+        try {
+            loginButton.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости кнопки входа");
+            throw new AssertionError(e.getMessage());
+        }
+    }
+
+    @Step("Проверяем видимость ссылки 'Забыли пароль?'")
+    public void verifyForgotPasswordLinkIsVisible() {
+        try {
+            forgotPasswordLink.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости ссылки 'Забыли пароль?'");
+            throw new AssertionError(e.getMessage());
+        }
+    }
+
+    @Step("Проверяем видимость кнопки регистрации")
+    public void verifyRegistrationButtonIsVisible() {
+        try {
+            registrationButton.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости кнопки регистрации");
+            throw new AssertionError(e.getMessage());
+        }
+    }
+
+    @Step("Проверяем видимость кнопки авторизации VK")
+    public void verifyVkAuthButtonIsVisible() {
+        try {
+            vkButton.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости кнопки авторизации VK");
+            throw new AssertionError(e.getMessage());
+        }
+    }
+
+    @Step("Проверяем видимость кнопки авторизации Yandex")
+    public void verifyYandexAuthButtonIsVisible() {
+        try {
+            yandexButton.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости кнопки авторизации Yandex");
+            throw new AssertionError(e.getMessage());
+        }
+    }
+
+    @Step("Проверяем видимость кнопки авторизации Mail.ru")
+    public void verifyMailRuAuthButtonIsVisible() {
+        try {
+            mailRuButton.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости кнопки авторизации Mail.ru");
+            throw new AssertionError(e.getMessage());
+        }
+    }
+
+    @Step("Проверяем видимость QR-кода")
+    public void verifyQrCodeButtonIsVisible() {
+        try {
+            qrCodeButton.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости QR-кода");
+            throw new AssertionError(e.getMessage());
+        }
+    }
+
+    @Step("Проверяем видимость кнопки восстановления аккаунта")
+    public void verifyRestoreButtonIsVisible() {
+        try {
+            restoreButton.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости кнопки восстановления аккаунта");
+            throw new AssertionError(e.getMessage());
+        }
+    }
+
+    @Step("Проверяем видимость поля поиска")
+    public void verifyInputSearchIsVisible() {
+        try {
+            inputSearch.shouldBe(visible);
+        } catch (AssertionError e) {
+            System.out.println("Ошибка видимости поля поиска");
+            throw new AssertionError(e.getMessage());
+        }
     }
 
     @Step("Проверяем видимость сообщение об ошибке входа")
